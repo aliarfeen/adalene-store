87f97cf0a02bf8ae945762b2c614a425ea479274
Nourhan-Reda <nourreda276@gmail.com>
2025-10-11 11:16:26 +0300

    route

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,34 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import 'flowbite';
+import { HomePage } from "./Pages/Home/HomePage.tsx";
+import { NotFound } from "./Pages/NotFound/NotFound.tsx";
+import ProductListPage from "./Pages/Products/ProductList.tsx";
 // Create a client
 const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <HomePage /> }, 
+      {path:"/products", element: <ProductListPage /> }, 
+  
+      { path: "*", element: <NotFound /> },
+    ]
+  }
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+ <RouterProvider router={router} />
     </QueryClientProvider>
   </StrictMode>
 );
+
+
+
