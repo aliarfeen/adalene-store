6cac97a6ab7c412d1355aca34942f0ae9b508780
Ali Hesham <63455367+aliarfeen@users.noreply.github.com>
2025-10-14 06:55:09 +0300

    added scroll to top effect

---
+++ src/Components/Common/ScrollToTop.tsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+ useEffect(() => {
+  const timeout = setTimeout(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, 50); // wait for render
+  return () => clearTimeout(timeout);
+}, [pathname]);
+
+  return null;
+}
--- src/Pages/Products/ProductDetails.tsx
+++ src/Pages/Products/ProductDetails.tsx
@@ -1,18 +1,30 @@
-import React from 'react'
-import ProductPrieview from '../../Components/ProductDetails/ProductPreview'
-import InfoContainer from '../../Components/ProductDetails/InfoColumn'
-import type { Product } from '../../Types';
+import React from "react";
+import ProductPrieview from "../../Components/ProductDetails/ProductPreview";
+import InfoContainer from "../../Components/ProductDetails/InfoColumn";
+import type { Product } from "../../Types";
+import ScrollToTop from "../../Components/Common/ScrollToTop";
 
-const ProductDetails = () => {  
-  const product : Product = JSON.parse(localStorage.getItem("product") || '{}');
+const ProductDetails = () => {
+  const product: Product = JSON.parse(localStorage.getItem("product") || "{}");
 
-    
   return (
     <div>
-      <ProductPrieview  bestSeller={product.bestSeller} resource={'product'} id={product.id} title={product.title} price={product.price} description={product.description} image={product.image} category={''} quantity={product.quantity}      />
+      <ScrollToTop />
+      <ProductPrieview
+        bestSeller={product.bestSeller}
+        resource={"product"}
+        id={product.id}
+        title={product.title}
+        price={product.price}
+        description={product.description}
+        image={product.image}
+        category={""}
+        quantity={product.quantity}
+        orderQuantity={0}
+      />
       <InfoContainer />
     </div>
-  )
-}
+  );
+};
 
-export default ProductDetails
+export default ProductDetails;
