1720d85ccd4e537c4fc905ea07cb8e45eba1e661
alaaahmed35 <alahaa35@gmail.com>
2025-10-24 23:56:56 +0300

    add wish list ui

---
+++ src/Pages/Profile/Wishlist.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import type { Product } from "../../Types/Product";
+import ProductCard from "../../Components/Products/ProductCard";
+import ScrollToTop from "../../Components/Common/ScrollToTop";
+import Pagination from "../../Components/Products/Pagination"; 
+
+const Wishlist: React.FC = () => {
+  const [favorites, setFavorites] = useState<Product[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const itemsPerPage = 6; 
+
+  useEffect(() => {
+    const loggedUser = JSON.parse(localStorage.getItem("loggedUser") || "{}");
+    if (loggedUser && Array.isArray(loggedUser.favourites)) {
+      setFavorites(loggedUser.favourites);
+    } else {
+      setFavorites([]);
+    }
+  }, []);
+
+
+  const lastItemIndex = currentPage * itemsPerPage;
+  const firstItemIndex = lastItemIndex - itemsPerPage;
+  const currentItems = favorites.slice(firstItemIndex, lastItemIndex);
+
+  return (
+    <>
+      <ScrollToTop />
+      <div className="max-w-6xl mx-auto bg-white m-1 shadow-sm p-6">
+        {favorites.length === 0 ? (
+          <p className="text-center text-[#6b4a33] text-lg">
+            Your wishlist is empty.
+          </p>
+        ) : (
+          <>
+            <div
+              className="grid gap-8
+                        grid-cols-1
+                        sm:grid-cols-2
+                        lg:grid-cols-3
+                        justify-items-center"
+            >
+              {currentItems.map((product) => (
+                <div
+                  key={product.id}
+                  className="w-full max-w-sm bg-[#fffdfa] border border-[#f0e6df] rounded-lg shadow-sm hover:shadow-md transition-all duration-200"
+                >
+                  <ProductCard product={product} />
+                </div>
+              ))}
+            </div>
+
+            {/* Pagination */}
+            <div className="flex justify-center mt-8">
+              <Pagination
+                totalitems={favorites.length}
+                itemsPerPage={itemsPerPage}
+                setCurrentPage={setCurrentPage}
+                currentPage={currentPage}
+              />
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Wishlist;
