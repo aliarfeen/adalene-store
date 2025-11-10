b1094cd7b76e0763f9e869ec51189dedbf4e58d6
Nourhan-Reda <nourreda276@gmail.com>
2025-11-10 18:24:23 +0200

    chatbot ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom"
 import { Footer } from "./Components/layout/Footer"
 import { Navbar } from "./Components/layout/Navbar"
+import ChatBot from "./Components/Chat/Chat"
 
 
 
@@ -10,9 +11,9 @@ export const App = () => {
   return (
     <div>
       <Navbar />
-      <Outlet/>
+      <Outlet />
       <Footer />
-
+      <ChatBot />
     </div>
   )
 }
